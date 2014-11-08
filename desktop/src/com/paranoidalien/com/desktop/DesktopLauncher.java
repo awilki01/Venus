4e4626550e4f4b1cab4349166ff32934bd99f4fe
awilki01 <adam.wilkins@mygrande.net>
2014-11-07 19:31:34 -0600

    initial commit

---
+++ desktop/src/com/paranoidalien/com/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.paranoidalien.com.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.paranoidalien.com.Venus;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Venus(), config);
+	}
+}
---
+++ html/src/com/paranoidalien/com/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.paranoidalien.com.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.paranoidalien.com.Venus;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Venus();
+        }
+}
